edd12c101b52fb6b69c362adaf06d43c42ab6f25
Clement-Lnrd <clement.lienard@epitech.eu>
2023-10-03 20:34:10 +0200

    refactor: refactor Mango code
    Remove useless parentheses in if/while
    Add spaces between functions
    Some function return values are assigned to variables instead of calling them multiple times
    Also remove update (check) features
    And some other minor changes

--- mango.py
+++ mango.py
@@ -4,7 +4,7 @@ from sys import argv, stderr
 from os import ttyname, system, path, walk
 from argparse import ArgumentParser as parseArgs
 from shutil import which
-from requests import get
+# from requests import get
 from time import sleep
 
 version = "v1.7.0"
@@ -32,30 +32,30 @@ mango_ascii = """                                                      \33[48;5;
        \33[48;5;003m      \33[48;5;100m     \33[48;5;136m                \33[48;5;130m  \33[48;5;094m      \33[0m
           \33[48;5;003m       \33[48;5;100m      \33[48;5;136m \33[3 \33[3 \33[3 \33[48;5;130m   \33[48;5;094m      \33[0m\n\n"""
 
-mango_tty = "    Sad TTY not handle well colors :(\n"\
-"                                                        %\n"\
-"                                                      (#%\n"\
-"                               (((((((((((((######   (#%\n"\
-"                           ((((((((((((((((((((######%%%%%%\n"\
-"                        (//((((((((((((((((((((((####%%%%%%%%%%\n"\
-"                      /////////////(((((((((((((((####%%%%%%%%%%%\n"\
-"                    //////////////////((((((((((((((####%%%%%%%%%%\n"\
-"                  %/////******///////////((((((((((((####%%%%%%%%%%\n"\
-"                 ///******,*****//////////((((((((((((####%%%%%%%%%&\n"\
-"                //****,,,,,,,***///////////((((((((((((###%%%%%%%%%%\n"\
-"               /****,,,,..,,,***///////////(((((((((((((###%%%%%%%%&\n"\
-"             %*****,,,,,,,,,****///////////(((((((((((((###%%%%%%%%&\n"\
-"            ////****,,,,,,****////////////((((((((###(((####%%%%%%%%\n"\
-"          ///////************////////////((((((((######(####%%%%%%%%\n"\
-"        %//////////******///////////////((((((((########%###%%%%%%%%\n"\
-"       ///////////////////////////////(((((((((#######%%%%   %%%%%%%&\n"\
-"     ((((////////////////////////////((((((((########%%%%          %%\n"\
-"    ((((((((((((//////////////////(((((((((########%%%%\n"\
-"    (((((((((((((((((((////(/((((((((((((########%%%%\n"\
-"    #######(((((((((((((((((((((((((((#########%%%\n"\
-"     ##########(((((((((((((((((((##########%%#\n"\
-"      %###############(((((##############%\n"\
-"          #########################(\n\n"
+mango_tty = "    Sad TTY not handle well colors :(\n" \
+            "                                                        %\n" \
+            "                                                      (#%\n" \
+            "                               (((((((((((((######   (#%\n" \
+            "                           ((((((((((((((((((((######%%%%%%\n" \
+            "                        (//((((((((((((((((((((((####%%%%%%%%%%\n" \
+            "                      /////////////(((((((((((((((####%%%%%%%%%%%\n" \
+            "                    //////////////////((((((((((((((####%%%%%%%%%%\n" \
+            "                  %/////******///////////((((((((((((####%%%%%%%%%%\n" \
+            "                 ///******,*****//////////((((((((((((####%%%%%%%%%&\n" \
+            "                //****,,,,,,,***///////////((((((((((((###%%%%%%%%%%\n" \
+            "               /****,,,,..,,,***///////////(((((((((((((###%%%%%%%%&\n" \
+            "             %*****,,,,,,,,,****///////////(((((((((((((###%%%%%%%%&\n" \
+            "            ////****,,,,,,****////////////((((((((###(((####%%%%%%%%\n" \
+            "          ///////************////////////((((((((######(####%%%%%%%%\n" \
+            "        %//////////******///////////////((((((((########%###%%%%%%%%\n" \
+            "       ///////////////////////////////(((((((((#######%%%%   %%%%%%%&\n" \
+            "     ((((////////////////////////////((((((((########%%%%          %%\n" \
+            "    ((((((((((((//////////////////(((((((((########%%%%\n" \
+            "    (((((((((((((((((((////(/((((((((((((########%%%%\n" \
+            "    #######(((((((((((((((((((((((((((#########%%%\n" \
+            "     ##########(((((((((((((((((((##########%%#\n" \
+            "      %###############(((((##############%\n" \
+            "          #########################(\n\n"
 
 types = [" INFO", " MINOR", " MAJOR"]
 
@@ -93,47 +93,60 @@ descriptions = [
     ["Line break at the end of file\33[0m", "Files must end with a line break."]
 ]
 
+
 def set_arguments():
     parser = parseArgs()
-    parser.add_argument("-Ee", "-Eerrors", "--exclude-errors", nargs = '+', help = "Exclude coding style errors from report")
-    parser.add_argument("-Ef", "-Efiles", "--exclude-files", nargs = '+', help = "Exclude files from coding style checking")
-    parser.add_argument("-w", "--watch", nargs = 1, help = "Watch for changes in the repository every x seconds")
-    parser.add_argument("-v", "--version", action = "store_true", help = "Show Mango version")
-    parser.add_argument("-u", "--update", action = "store_true", help = "Update Mango to last version")
+    parser.add_argument("-Ee", "-Eerrors", "--exclude-errors", nargs='+',
+                        help="Exclude coding style errors from report")
+    parser.add_argument("-Ef", "-Efiles", "--exclude-files", nargs='+',
+                        help="Exclude files from coding style checking")
+    parser.add_argument("-w", "--watch", nargs=1,
+                        help="Watch for changes in the repository every x seconds")
+    parser.add_argument("-v", "--version", action="store_true",
+                        help="Show Mango version")
+    parser.add_argument("-u", "--update", action="store_true",
+                        help="Update Mango to last version (actually not working)")
     return parser.parse_args()
+
 
 def download_coding_style():
     print("Downloading coding-style...\n")
     system(f"sudo wget https://raw.githubusercontent.com/Clement-Z4RM/coding-style-checker/main/coding-style.sh -O /bin/coding-style 2> /dev/null && sudo chmod +x /bin/coding-style")
 
-def check_for_update(args):
-    try:
-        response = get("https://api.github.com/repos/Clement-Z4RM/Mango/releases/latest")
-        if (response):
-            new_version = response.json()["name"]
-        if (not args.update and new_version != version):
-            print("New version of Mango available. You can update it doing \"\33[3mmango -u, --update\33[0m\".\n")
-    except:
-        if (args.update):
-            print("Cannot get latest version, please verify your internet connection.\n", file=stderr)
-        else:
-            print("Cannot check for update, please verify your internet connection.\n", file=stderr)
+# def check_for_update(args):
+#     try:
+#         response = get("https://api.github.com/repos/Clement-Z4RM/Mango/releases/latest")
+#         if response:
+#             new_version = response.json()["name"]
+#         if not args.update and new_version != version:
+#             print(
+#                 "New version of Mango available. You can update it doing \"\33[3mmango -u, --update\33[0m\".\n")
+#     except:
+#         if args.update:
+#             print("Cannot get latest version, please verify your internet connection.\n",
+#                   file=stderr)
+#         else:
+#             print("Cannot check for update, please verify your internet connection.\n", file=stderr)
+
 
 def update():
-    system("sudo wget https://raw.githubusercontent.com/Clement-Z4RM/Mango/main/mango.py -O /bin/mango 2> /dev/null && sudo chmod +x /bin/mango")
-    system("curl -s https://pastebin.com/raw/baCbaUqU")
+    # system("sudo wget https://raw.githubusercontent.com/Clement-Z4RM/Mango/main/mango.py -O /bin/mango 2> /dev/null && sudo chmod +x /bin/mango")
+    # system("curl -s https://pastebin.com/raw/baCbaUqU")
+    print("Updating not working at the moment, it will be re-introduced in a future version.")
     exit()
+
 
 def print_version():
     print(f"Mango {version}")
     exit()
 
+
 def get_exclude_files(args):
     exclude = []
-    if (not args.exclude_files):
+    if not args.exclude_files:
         return exclude
     for file in args.exclude_files:
-        if (path.isdir(file)):
+        if path.isdir(file):
             for root, subdirs, files in walk(file):
                 for filename in files:
                     file = path.join(root, filename)
@@ -144,13 +157,15 @@ def get_exclude_files(args):
             exclude.append(file.strip())
     return exclude
 
+
 def get_exclude_errors(args):
     exclude = []
-    if (not args.exclude_errors):
+    if not args.exclude_errors:
         return exclude
     for error in args.exclude_errors:
         exclude.append(error.strip())
     return exclude
+
 
 def coding_style():
     system("sudo rm -f /tmp/coding-style-reports.log")
@@ -165,47 +180,51 @@ def coding_style():
     out = out.split('\n')
     return out
 
+
 def print_error(line, index):
     color = ("\33[0m" + colors[types.index(line[2])])
-    print(f"{color}\33[1m{line[0]}", end = '')
-    if (int(line[1]) > 1):
-        print(f" at line {line[1]}", end = '')
-    print(f"\n{line[2][1:]} {line[3]}: {color}{descriptions[index][0]}\n{descriptions[index][1]}\n")
+    print(f"{color}\33[1m{line[0]}", end='')
+    if int(line[1]) > 1:
+        print(f" at line {line[1]}", end='')
+    print(f"\n{line[2][1:]} {line[3]}: {color}{descriptions[index][0]}\33[0m\n{descriptions[index][1]}\n")
 
-def print_errors_number(err_nb):
-    if (err_nb[2] > 0 or err_nb[1] > 0 or err_nb[0] or err_nb[3] > 0):
-        print(f"\33[1;31m{err_nb[2]} Major\33[0m | \33[1;33m{err_nb[1]} Minor\33[0m | \33[1;36m{err_nb[0]} Info\33[0m", end = '')
-        if (err_nb[3] > 0):
-            print(f" | \33[1;37m{err_nb[3]} Unknown\33[0m", end = '')
-        if (err_nb[4] > 0):
-            print(f" | \33[1;37m{err_nb[4]} excluded\33[0m", end = '')
+
+def print_errors_number(err_nb, is_a_tty):
+    if err_nb[2] > 0 or err_nb[1] > 0 or err_nb[0] or err_nb[3] > 0:
+        print(f"\33[1;31m{err_nb[2]} Major\33[0m | \33[1;33m{err_nb[1]} Minor\33[0m | \33[1;36m{err_nb[0]} Info\33[0m",
+              end='')
+        if err_nb[3] > 0:
+            print(f" | \33[1;37m{err_nb[3]} Unknown\33[0m", end='')
+        if err_nb[4] > 0:
+            print(f" | \33[1;37m{err_nb[4]} excluded\33[0m", end='')
         print()
-    elif (not is_a_tty):
+    elif not is_a_tty:
         print(f"{mango_ascii}    ✅ There is no coding style error")
-        if (err_nb[4] > 0):
+        if err_nb[4] > 0:
             print(f"\nBe careful, you still have {err_nb[4]} excluded errors")
     else:
         print(f"{mango_tty}    but you have no coding style error :)")
-        if (err_nb[4] > 0):
+        if err_nb[4] > 0:
             print(f"\nBe careful, you still have {err_nb[4]} excluded errors")
 
-def mango(exclude_files, exclude_errors, watch):
+
+def mango(exclude_files, exclude_errors):
     out = coding_style()
-    if (watch):
-        system("clear")
     err_nb = [0, 0, 0, 0, 0]
-    if (not out):
-        if (not is_a_tty):
+    is_a_tty = "tty" in ttyname(1)
+
+    if not out:
+        if not is_a_tty:
             print(f"{mango_ascii}    ✅ There is no coding style error")
         else:
             print(f"{mango_tty}    but you have no coding style error :)")
         return
     for line in out:
         backup_line = line
-        if (line == ''):
+        if line == '':
             continue
         line = line.split(':')
-        if (path.abspath(line[0]) in exclude_files or line[3] in exclude_errors):
+        if path.abspath(line[0]) in exclude_files or line[3] in exclude_errors:
             err_nb[4] += 1
             continue
         try:
@@ -214,7 +233,7 @@ def mango(exclude_files, exclude_errors, watch):
             print(f"{backup_line}\n")
             err_nb[3] += 1
             continue
-        if (line[0][0:2] == "./"):
+        if line[0][0:2] == "./":
             line[0] = line[0][2:]
         match line[2]:
             case " INFO":
@@ -224,29 +243,36 @@ def mango(exclude_files, exclude_errors, watch):
             case " MAJOR":
                 err_nb[2] += 1
         print_error(line, index)
-    print_errors_number(err_nb)
+    print_errors_number(err_nb, is_a_tty)
+
 
 def main():
     args = set_arguments()
-    if (args.version):
+
+    if args.version:
         print_version()
-    check_for_update(args)
-    if (args.update):
+    if args.update:
         update()
-    if (which("coding-style") is None):
+    if which("coding-style") is None:
         download_coding_style()
-    if (args.watch):
-        while (True):
+
+    exclude_files = get_exclude_files(args)
+    exclude_errors = get_exclude_errors(args)
+
+    if args.watch:
+        sleep_duration = int(args.watch[0])
+        while True:
             try:
-                mango(get_exclude_files(args), get_exclude_errors(args), args.watch)
-                sleep(int(args.watch[0]))
+                system("clear")
+                mango(exclude_files, exclude_errors)
+                sleep(sleep_duration)
             except KeyboardInterrupt:
                 exit()
             except:
                 exit(84)
     else:
-        mango(get_exclude_files(args), get_exclude_errors(args), args.watch)
+        mango(exclude_files, exclude_errors)
+
 
 if __name__ == "__main__":
-    is_a_tty = ttyname(1).split('/')[-1].startswith("tty")
     main()
