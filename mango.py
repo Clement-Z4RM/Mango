30c2b4c0318b988ded9a358ae8f84c19618f37ec
Clement-Lnrd <clement.lienard@epitech.eu>
2023-12-28 18:25:18 +0100

    refactor: transform `colors` and `description` arrays into dictionaries

--- mango.py
+++ mango.py
@@ -63,10 +63,12 @@ types = [" INFO", " MINOR", " MAJOR", " FATAL"]
 
 colors = ["\33[36m", "\33[33m", "\33[31m", "\33[31m"]
 
-rules = ["C-O1", "C-O3", "C-O4", "C-G1", "C-G2", "C-G3", "C-G4", "C-G5", "C-G6", "C-G7", "C-G8",
-         "C-G10", "C-F2", "C-F3", "C-F4", "C-F5", "C-F6", "C-F7", "C-F8", "C-F9", "C-L1", "C-L2",
-         "C-L3", "C-L4", "C-L5", "C-L6", "C-V1", "C-V3", "C-C1", "C-C2", "C-C3", "C-H1", "C-H2",
-         "C-H3", "C-A3"]
+colors = {
+    " INFO": "\33[36m",
+    " MINOR": "\33[33m",
+    " MAJOR": "\33[31m",
+    " FATAL": "\33[31m"
+}
 
 descriptions = [
     ["Contents of the repository",
@@ -127,13 +129,123 @@ descriptions = [
     ["Macros", "Macros must match only one statement, and fit on a single line."],
     ["Line break at the end of file", "Files must end with a line break."]
 ]
+__descriptions = {
+    "O1": [
+        "Contents of the repository",
+        "The repository should contain only files required for compilation and must not contain compiled (\33[3m.o, .hi, .a, .so, ...\33[3m), temporary or unnecessary files  (\33[3m*~ * #, *.d, toto, ...\33[3m)."
+    ]
+}
+
+descriptions = {
+    "C-O1": __descriptions["O1"],
+    "C-O3": [
+        "File coherence",
+        "A source file mustn't contain more than 10 functions (including at most 5 non-static functions)."
+    ],
+    "C-O4": [
+        "Naming files and folders",
+        "All files names and folders must be in English, according the \33[3msnake_case\33[0m convention."
+    ],
+    "C-G1": [
+        "File header",
+        "C files and every Makefile must always start with the standard header of the school."
+    ],
+    "C-G2": [
+        "Separation of functions",
+        "Inside a source file, implementations of functions must be separated by one and only one empty line."
+    ],
+    "C-G3": [
+        "Indentation of reprocessor directives",
+        "The preprocessor directives must be indented according to the level of indirection."
+    ],
+    "C-G4": [
+        "Global variables",
+        "Global variables must be avoided as much as possible. Only global constants should be used."
+    ],
+    "C-G5": ["\33[3minclude", "\33[3minclude\33[0m directive must only include C header files."],
+    "C-G6": ["Line endings", "Line endings must be done in UNIX style (with \33[3m\\n\33[0m)."],
+    "C-G7": ["Trailing spaces", "No trailing spaces must be present at the end of a line."],
+    "C-G8": [
+        "Leading/trailing lines",
+        "No leading empty lines must be present. No more than 1 trailing empty line must be present."
+    ],
+    "C-G10": [
+        "Inline assembly",  # Celui qui a cette erreur s'est perdu MDR
+        "Inline assembly must never be used. Programming in C must be done... in C."
+    ],
+    "C-F2": [
+        "Naming functions",
+        "The name of a function must define the task it executes and must contain a verb. All function names must be in English, according to the \33[3msnake_case\33[0m convention."
+    ],
+    "C-F3": ["Number of columns", "The length of a line must not exceed 80 columns."],
+    "C-F4": [
+        "Number of lines",
+        "The body of a function should be as short as possible, and must not exceed 20 lines."
+    ],
+    "C-F5": ["Number of parameters", "A function must not have more than 4 parameters."],
+    "C-F6": [
+        "Functions without parameters",
+        "A function taking no parameters must take void as a parameter in the function declaration."
+    ],
+    "C-F7": [
+        "Structures as parameters",
+        "Structures must be passed to functions using a pointer, not by copy."
+    ],
+    "C-F8": ["Comments inside a function", "There must be no comment within a function."],
+    "C-F9": ["Nested functions", "Nested functions are not allowed."],
+    "C-L1": ["Code line content", "A line must correspond to only one statement."],
+    "C-L2": [
+        "Indentation",
+        "Each indentation level must be done by using 4 spaces. No tabulations may be used for indentation."
+    ],
+    "C-L3": [
+        "Spaces",
+        "When using a space as a separator, one and only one space character must be used."
+    ],
+    "C-L4": [
+        "Curly brackets",
+        "Opening curly brackets must be at the end of the line, after the content it precedes, except for functions definitions where they must be placed alone on their line. Closing curly brackets must be alone on their line, except in the case of \33[3melse\33[0m/\33[3melse if\33[0m control structures, \33[3menum\33[0m declarations, or structure declarations."
+    ],
+    "C-L5": [
+        "Variable declarations",
+        "Variables must be declared at the beginning of the function. Only one variable must be declared per statement."
+    ],
+    "C-L6": [
+        "Blank lines",
+        "A blank line must separate the variable declarations from the remainder of the function. No other blank lines must be present in the function."
+    ],
+    "C-V1": [
+        "Naming identifiers",
+        "All identifier names must be in English, according to the \33[3msnake_case\33[0m convention. The type names defined with \33[3mtypedef\33[0m must end with \33[3m_t\33[0m. The names of macros and global constants and the content of enums must be written in \33[3mUPPER_SNAKE_CASE\33[0m."
+    ],
+    "C-V3": [
+        "Pointers",
+        "The pointer symbol (*) must be attached to the associated variable, with no spaces in between. It must also be preceded by a space, except when it is itself preceded by another asterisk. When used in a cast, the asterisk must have a space on its left side, but not on its right side."
+    ],
+    "C-C1": [
+        "Conditional branching",
+        "A conditionnal block must not contain more than 3 branches."
+    ],
+    "C-C2": [
+        "Ternary operators",
+        "The use of ternary operators is allowed as far as it is kept simple and readable, and if it does not obfuscate code. You must never use nested or chained ternary operators. You must always use the value produced by a ternary operator (by assigning it to a variable or returning it for example)."
+    ],
+    "C-C3": ["\33[3mgoto", "Using the \33[3mgoto\33[0m keyword if forbidden."],
+    "C-H1": [
+        "Content",
+        "Header files must only contain \33[1mfunctions prototypes\33[0m, \33[1mtypes declarations\33[0m, \33[1mglobal variable/constant declarations\33[0m, \33[1mmacros\33[0m, \33[1mstatic inline functions\33[0m. All these elements must only be found in header files, and thus not in source files."
+    ],
+    "C-H2": ["Include guard", "Headers must be protected from double inclusion."],
+    "C-H3": ["Macros", "Macros must match only one statement, and fit on a single line."],
+    "C-A3": ["Line break at the end of file", "Files must end with a line break."],
 
 
 def set_arguments():
     parser = parseArgs()
     parser.add_argument("-Ee", "-Eerrors", "--exclude-errors", nargs='+',
+    parser.add_argument("-Ee", "-Eerrors", "--exclude-errors", nargs="+",
                         help="Exclude coding style errors from report")
-    parser.add_argument("-Ef", "-Efiles", "--exclude-files", nargs='+',
+    parser.add_argument("-Ef", "-Efiles", "--exclude-files", nargs="+",
                         help="Exclude files from coding style checking")
     parser.add_argument("-w", "--watch", nargs=1,
                         help="Watch for changes in the repository every x seconds")
@@ -152,12 +264,13 @@ def download_coding_style():
         print(" and neither \33[3wget\33[0m nor \33[3curl\33[0m were found on your computer,"
               f" so please download \33[3mcoding-style\33[0m here: {CODING_STYLE_URL}"
               f" and/or add it to your path")
-    print(" download it? [Y/n] ", end='')
+    print(" download it? [Y/n] ", end="")
 
     key = input().lower()
 
     if key not in ('', 'y', 'yes', 'o', 'oui'):
         print("You must have the \33[3mcoding-style\33[0m command to be able to use \33[1mMango\33[0m")
+    if key not in ("", "y", "yes", "o", "oui"):
         sys.exit(1)
     print("Downloading \33[3mcoding-style\33[0m...")
 
@@ -183,7 +296,7 @@ def get_exclude_files(args):
         return exclude
     for file in args.exclude_files:
         if path.isdir(file):
-            for root, _subdirs, files in walk(file):
+            for root, _dirs, files in walk(file):
                 for filename in files:
                     file = path.join(root, filename)
                     file = path.abspath(file)
@@ -217,24 +330,24 @@ def coding_style():
     return out
 
 
-def print_error(line, index):
-    color = f"\33[0m{colors[types.index(line[2])]}"
-    print(f"{color}\33[1m{line[0]}", end='')
+def print_error(line, rule):
+    color = f"\33[0m{colors[line[2]]}"
+    print(f"{color}\33[1m{line[0]}", end="")
     if int(line[1]) > 1:
-        print(f" at line {line[1]}", end='')
-    print(f"\n{line[2][1:]} {line[3]}: {color}{descriptions[index][0]}\33[0m\n{descriptions[index][1]}\n")
+        print(f" at line {line[1]}", end="")
+    print(f"\n{line[2][1:]} {line[3]}: {color}{rule[0]}\33[0m\n{rule[1]}\n")
 
 
 def print_errors_number(err_nb, is_a_tty):
     if err_nb[3] or err_nb[2] > 0 or err_nb[1] > 0 or err_nb[0] or err_nb[4] > 0:
         if err_nb[3]:
-            print(f"\33[1;31m{err_nb[3]} Fatal\33[0m | ", end='')
+            print(f"\33[1;31m{err_nb[3]} Fatal\33[0m | ", end="")
         print(f"\33[1;31m{err_nb[2]} Major\33[0m | \33[1;33m{err_nb[1]} Minor\33[0m | \33[1;36m{err_nb[0]} Info\33[0m",
-              end='')
+              end="")
         if err_nb[4] > 0:
-            print(f" | \33[1;37m{err_nb[4]} Unknown\33[0m", end='')
+            print(f" | \33[1;37m{err_nb[4]} Unknown\33[0m", end="")
         if err_nb[5] > 0:
-            print(f" | \33[1;37m{err_nb[5]} excluded\33[0m", end='')
+            print(f" | \33[1;37m{err_nb[5]} excluded\33[0m", end="")
         print()
     elif not is_a_tty:
         print(f"{MANGO_ASCII}    ✅ There is no coding style error")
@@ -263,7 +376,7 @@ def mango(exclude_files, exclude_errors):
         return
     for line in out:
         backup_line = line
-        if line == '':
+        if line == "":
             continue
         line = line.split(':')
         if path.abspath(line[0]) in exclude_files or line[3] in exclude_errors:
@@ -271,6 +384,7 @@ def mango(exclude_files, exclude_errors):
             continue
         try:
             index = rules.index(line[3])
+            rule = descriptions[line[3]]
         except:
             print(f"{backup_line}\n")
             err_nb[4] += 1
@@ -286,7 +400,7 @@ def mango(exclude_files, exclude_errors):
                 err_nb[2] += 1
             case " FATAL":
                 err_nb[3] += 1
-        print_error(line, index)
+        print_error(line, rule)
     print_errors_number(err_nb, is_a_tty)
 
 
